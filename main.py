94440fe0e1770a0e3e9532b46aaff39bd0f06a0e
Steven <stevenbutter.451@gmail.com>
2020-10-26 17:02:53 +0000

    refactor read operation into own file

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-from openpyxl import load_workbook
+import os
 
-# column 10 is missing in customer_copy
-
-
-def GetDictForRow(row, sheet):
-    return {customerSheet.cell(1, cell.column).value: cell.value for cell in row}
-
+from SourceXlsReader import GetWorkbookAsDictionary
 
 if __name__ == "__main__":
-    customerWorkbook = load_workbook('customer_copy.xlsx')
-    customerSheet = customerWorkbook.active
+    srcData = GetWorkbookAsDictionary(
+        os.path.join('test', 'customer_copy.xlsx'))
 
-    rows = [GetDictForRow(row, customerSheet)
-            for row in customerSheet.iter_rows(min_row=2)]
+    print(srcData)
